d74052d833fe93826290e15f4275eaaacd48d0b6
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2025-04-19 16:17:31 +0700

    init project

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const axios = require('axios')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = process.env.PORT || 3000
+
+const botToken = process.env.BOT_TOKEN
+const chatId = process.env.CHAT_ID
+const topicId = process.env.TOPIC_ID
+
+app.use(bodyParser.json());
+
+
+app.post('/mqtt-webhook', async (req, res) => {
+    try {
+        const message = req.body;
+
+        const clientConnectMessage = `Client connected: ${message.clientid}`;
+
+        await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            chat_id: chatId,
+            text: clientConnectMessage,
+            message_thread_id: topicId
+        });
+
+        res.status(200).send('Notification sent to Telegram');
+    } catch (error) {
+        console.error('Error sending message to Telegram:', error);
+        res.status(500).send('Failed to send message');
+    }
+});
+app.get('/test', async (req, res) => {
+    res.status(200).send('Notification sent to Telegram');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
